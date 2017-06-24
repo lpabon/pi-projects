4c7d856da29f570fd0d6b207724d9abe5bbce1e2
Luis Pabón <lpabon@gmail.com>
2017-06-24 12:49:23 -0400

    snow

---
+++ sensehat/snowydays.py
@@ -0,0 +1,390 @@
+from sense_hat import SenseHat
+from time import sleep
+sense=SenseHat()
+
+c = (0, 255, 255)
+b = (0, 0, 255)
+e = (0, 0, 0)
+g = (0, 255, 0)
+f = (255, 0, 255)
+o = (255, 128, 0)
+p = (102, 0, 204)
+r = (255, 0, 0)
+w = (255, 255, 255)
+y = (255, 255, 0)
+
+
+
+frame1 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame2 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame3 = [e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame4 = [e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame5 = [b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame6 = [b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame7 = [b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame8 = [b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame9 = [b,b,b,b,b,e,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame10 = [b,b,b,b,b,e,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame11 = [b,b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame12 = [b,b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame13 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame14 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame15 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame16 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame17 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,w,e,e,e,e,g,e,w,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame18 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,w,e,e,e,e,g,e,w,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame19 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,w,e,e,e,e,g,e,w,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame20 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,e,e,e,e,e,e,e,e,e,g,e,w,e,e,e,e,g,e,w,e,e,g,w,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame21 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,e,e,e,e,e,e,e,e,e,g,e,w,e,e,e,e,g,e,w,e,e,g,w,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame22 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,w,e,e,e,e,g,e,w,e,e,g,w,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame23 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,w,e,e,e,e,g,e,w,e,e,g,w,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame24 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,e,e,e,e,e,g,e,w,e,e,g,w,e,g,e,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame25 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,e,e,e,e,e,g,e,w,e,e,g,w,e,g,e,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame26 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,e,e,e,e,e,g,e,w,e,e,g,w,e,g,e,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame27 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,w,e,e,w,e,e,e,e,e,e,w,e,g,e,e,e,e,e,e,g,e,w,e,e,g,w,e,g,e,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame28 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,e,e,e,w,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame29 = [b,b,b,b,b,b,b,b,e,w,e,e,e,e,w,e,e,e,e,w,e,e,e,e,w,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame30 = [b,b,b,b,b,b,b,b,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame31 = [b,b,b,b,b,b,b,b,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame32 = [b,b,b,b,b,b,b,b,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame33 = [b,b,b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame34 = [b,b,b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame35 = [b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame36 = [b,b,b,b,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame37 = [b,e,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame38 = [b,e,b,b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame39 = [b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame40 = [b,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame41 = [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame42 = [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame43 = [e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame44 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame45 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame46 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame47 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame48 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame49 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,w,e,g,w,w,w,e,g,g,w,w,w,w,w,w,w,w,w]
+
+frame50 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,e,e,g,w,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame51 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,w,e,g,e,e,g,w,w,w,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame52 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,w,w,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame53 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,w,w,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame54 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame55 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,w,w,w]
+
+frame56 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,g,g,g]
+
+frame57 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,w,w,w,g,g,g]
+
+frame58 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,g,g,g,g,g,g]
+
+frame59 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,g,g,g,g,g,g]
+
+frame60 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,w,w,g,g,g,g,g,g]
+
+frame61 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame62 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,e,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame63 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame64 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,e,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame65 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame66 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame67 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame68 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame69 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,o,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame70 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,o,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame71 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,o,e,e,e,e,e,e,e,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame72 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,o,e,e,e,e,e,e,p,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame73 = [e,e,e,e,e,e,y,y,e,e,e,e,e,e,y,y,e,e,e,e,e,e,e,e,e,o,e,e,e,e,e,e,p,g,e,e,e,f,e,e,g,e,e,e,e,g,f,e,g,e,e,e,e,g,g,e,g,g,g,g,g,g,g,g]
+
+frame74 = [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e]
+
+## This demo program will show each frame for 1 second ##
+sense.set_pixels(frame1)
+sleep(1)
+
+sense.set_pixels(frame2)
+sleep(1)
+
+sense.set_pixels(frame3)
+sleep(1)
+
+sense.set_pixels(frame4)
+sleep(1)
+
+sense.set_pixels(frame5)
+sleep(1)
+
+sense.set_pixels(frame6)
+sleep(1)
+
+sense.set_pixels(frame7)
+sleep(1)
+
+sense.set_pixels(frame8)
+sleep(1)
+
+sense.set_pixels(frame9)
+sleep(1)
+
+sense.set_pixels(frame10)
+sleep(1)
+
+sense.set_pixels(frame11)
+sleep(1)
+
+sense.set_pixels(frame12)
+sleep(1)
+
+sense.set_pixels(frame13)
+sleep(1)
+
+sense.set_pixels(frame14)
+sleep(1)
+
+sense.set_pixels(frame15)
+sleep(1)
+
+sense.set_pixels(frame16)
+sleep(1)
+
+sense.set_pixels(frame17)
+sleep(1)
+
+sense.set_pixels(frame18)
+sleep(1)
+
+sense.set_pixels(frame19)
+sleep(1)
+
+sense.set_pixels(frame20)
+sleep(1)
+
+sense.set_pixels(frame21)
+sleep(1)
+
+sense.set_pixels(frame22)
+sleep(1)
+
+sense.set_pixels(frame23)
+sleep(1)
+
+sense.set_pixels(frame24)
+sleep(1)
+
+sense.set_pixels(frame25)
+sleep(1)
+
+sense.set_pixels(frame26)
+sleep(1)
+
+sense.set_pixels(frame27)
+sleep(1)
+
+sense.set_pixels(frame28)
+sleep(1)
+
+sense.set_pixels(frame29)
+sleep(1)
+
+sense.set_pixels(frame30)
+sleep(1)
+
+sense.set_pixels(frame31)
+sleep(1)
+
+sense.set_pixels(frame32)
+sleep(1)
+
+sense.set_pixels(frame33)
+sleep(1)
+
+sense.set_pixels(frame34)
+sleep(1)
+
+sense.set_pixels(frame35)
+sleep(1)
+
+sense.set_pixels(frame36)
+sleep(1)
+
+sense.set_pixels(frame37)
+sleep(1)
+
+sense.set_pixels(frame38)
+sleep(1)
+
+sense.set_pixels(frame39)
+sleep(1)
+
+sense.set_pixels(frame40)
+sleep(1)
+
+sense.set_pixels(frame41)
+sleep(1)
+
+sense.set_pixels(frame42)
+sleep(1)
+
+sense.set_pixels(frame43)
+sleep(1)
+
+sense.set_pixels(frame44)
+sleep(1)
+
+sense.set_pixels(frame45)
+sleep(1)
+
+sense.set_pixels(frame46)
+sleep(1)
+
+sense.set_pixels(frame47)
+sleep(1)
+
+sense.set_pixels(frame48)
+sleep(1)
+
+sense.set_pixels(frame49)
+sleep(1)
+
+sense.set_pixels(frame50)
+sleep(1)
+
+sense.set_pixels(frame51)
+sleep(1)
+
+sense.set_pixels(frame52)
+sleep(1)
+
+sense.set_pixels(frame53)
+sleep(1)
+
+sense.set_pixels(frame54)
+sleep(1)
+
+sense.set_pixels(frame55)
+sleep(1)
+
+sense.set_pixels(frame56)
+sleep(1)
+
+sense.set_pixels(frame57)
+sleep(1)
+
+sense.set_pixels(frame58)
+sleep(1)
+
+sense.set_pixels(frame59)
+sleep(1)
+
+sense.set_pixels(frame60)
+sleep(1)
+
+sense.set_pixels(frame61)
+sleep(1)
+
+sense.set_pixels(frame62)
+sleep(1)
+
+sense.set_pixels(frame63)
+sleep(1)
+
+sense.set_pixels(frame64)
+sleep(1)
+
+sense.set_pixels(frame65)
+sleep(1)
+
+sense.set_pixels(frame66)
+sleep(1)
+
+sense.set_pixels(frame67)
+sleep(1)
+
+sense.set_pixels(frame68)
+sleep(1)
+
+sense.set_pixels(frame69)
+sleep(1)
+
+sense.set_pixels(frame70)
+sleep(1)
+
+sense.set_pixels(frame71)
+sleep(1)
+
+sense.set_pixels(frame72)
+sleep(1)
+
+sense.set_pixels(frame73)
+sleep(1)
+
+sense.set_pixels(frame74)
+sleep(1)
+
+
+sense.clear()
